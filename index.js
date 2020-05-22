ea4e8c66eafcc7398978c83e3202a3a672f09dfa
gokul-00 <62957549+gokul-00@users.noreply.github.com>
2020-05-22 20:15:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const background = document.querySelector("body");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3"
+  ];
+
+  pads.forEach((pad, index) => {
+    
+      
+      pad.addEventListener("mousedown",function(){
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        background.style.backgroundColor = colors[index]; 
+      })
+      pad.addEventListener("mouseup",function(){
+        
+        background.style.backgroundColor = "white"; 
+      })
+      pad.addEventListener("touchstart",function(){
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        background.style.backgroundColor = colors[index]; 
+      })
+      pad.addEventListener("touchend",function(){
+        
+        background.style.backgroundColor = "white"; 
+      })
+   
+  });
+
+  
+});
